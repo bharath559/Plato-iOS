6b08e0258e4843dfa8f9f330965b14accfd3478b
bkongara <bkongara@cs.odu.edu>
2016-02-22 17:03:41 -0500

    Initial Commit

---
+++ Plato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Plato
+//
+//  Created by Bharath Kongara on 2/22/16.
+//  Copyright © 2016 Bharath Kongara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
